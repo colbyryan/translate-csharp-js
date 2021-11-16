db086e4b6ca8cc513ddb8cb85929580a79fdd218
Colby Ryan <colbyr.ryan@gmail.com>
2021-11-16 10:10:46 -0600

    First Commit

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,21 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
   console.log(name);
+}
+
+console.log("")
+
+const theNames = names.filter(n => n.startsWith("The"))
+
+console.log("the Places Names")
+for (n of theNames) {
+  console.log(n)
 }
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,38 @@
 // Put your code here
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+
+for (let i = 0; i < 10; i++) {
+    die1 = Roll();
+    die2 = Roll();
+
+    let message = `${die1} + ${die2} == ${die1.Value + die2.Value}`
+    if (die1.Value === die2.Value) {
+        message += " DOUBLES!"
+    }
+
+    console.log(message)
+}
+
+const dieRoll = () => {
+    let dieValue = new Random().Next(1, 6)
+    let die = new Die(dieValue)
+    return (
+        die
+    )
+}
+
+
+const createDie = (value) => {
+    return {
+        value,
+        toString: () => {
+            let dieString = "unknown"
+            let dieStringArray = ["dummy", "one", "two", "three", "four", "five", "six"]
+            dieString = dieStringArray[this.value]
+            return dieString
+        }
+    }
+
+}
